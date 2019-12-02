0e271b541f426fdfcd67b3b39bda2173996b2f31
Jonatas <jbmagalhaes28@gmail.com>
2019-12-02 20:54:15 -0300

    Day 2 finished

--- day2/t2.hs
+++ day2/t2.hs
@@ -5,11 +5,15 @@ main :: IO ()
 main = do
   fileInput <- readFile "input.txt"
   let parsedInput = (read :: String -> Int) <$> splitOn "," (init fileInput)
-  let modInput = changeAt 1 12 $ changeAt 2 2 parsedInput
-  print (head $ execProgram modInput 0)
+  print $ problem2 parsedInput
+  print $ problem2B parsedInput 100 0
 
 changeAt :: Int -> a -> [a] -> [a]
 changeAt n a list = (\(l, _:xs) -> l ++ a:xs) $ splitAt n list
+
+-- Just to help the bruteforce
+updateSource :: [Int] -> Int -> Int -> [Int]
+updateSource source x y = changeAt 1 x $ changeAt 2 y source
 
 execOpCode :: Int -> Int -> [Int] -> [Int]
 execOpCode 1 at source = changeAt loc (lhs + rhs) source
@@ -28,3 +32,14 @@ execProgram source opPos
   | otherwise = execProgram (execOpCode opcode opPos source) nextOp
   where opcode = source !! opPos
         nextOp = opPos + 4
+
+-- Bruteforce Solution
+problem2 source = [(x, y) | x <- [1..99], y <- [1..99], head (execProgram (updateSource source x y) 0) == 19690720]
+
+-- Binary Search solution
+problem2B source high low
+  | 19690720 - result < 100 && 19690720 - result > 0 = (pivot, (19690720 - result))
+  | result < 19690720 = problem2B source high pivot
+  | otherwise = problem2B source pivot low
+  where pivot = ((high - low) `div` 2) + low
+        result = (head (execProgram (changeAt 1 pivot source) 0))
